8d26892f477441cf11f965a6f561db2d6e19f6a2
monstaro <monstaro32@gmail.com>
2020-01-27 16:12:25 -0700

    creates turn class and methods

---
+++ src/Turn.js
@@ -0,0 +1,20 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
+  returnGuess() {
+
+  }
+  returnCard() {
+
+  }
+  evaluateGuess() {
+
+  }
+  giveFeedback() {
+    
+  }
+}
+
+export default Turn
