856469a3fc4d4ffe8fe182dc3c4ea3b91f0fa6bb
monstaro <monstaro32@gmail.com>
2020-01-29 15:40:59 -0700

    Instantiates a deck and round

--- src/Game.js
+++ src/Game.js
@@ -2,13 +2,22 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('../src/Deck.js')
+const Round = require('../src/Round.js')
+const Card = require('../src/Card.js')
+
+let round;
 
 class Game {
   constructor() {
-    this.currentRound = util.getRound();
+    this.currentRound = null;
   }
   start() {
-
+    let deck = new Deck(data.prototypeData)
+    round = new Round(deck)
+    // this.currentRound = round
+    this.printMessage(deck, round)
+    this.printQuestion(round)
   }
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
